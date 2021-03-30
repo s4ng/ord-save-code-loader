d4dfb414c302ec17b5d4a37ed1b1561d939bf131
s4ng <zalcls951201@gmail.com>
2021-03-30 22:14:46 +0900

    update build option

---
+++ setup.py
@@ -0,0 +1,14 @@
+from cx_Freeze import setup, Executable
+
+buildOptions = dict(packages=['tkinter'])
+
+exe = [Executable('src/app.py', base='Win32GUI')]
+
+setup(
+    name='ordSaveCodeLoader', 
+    version="0.0.1",
+    author="s4ng",
+    description="원랜디 세이브 코드 로더",
+    options=dict(build_exe = buildOptions),
+    executables=exe
+)
