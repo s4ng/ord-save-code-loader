9fda700ec912ab9aced5e95f7185e4c514472b1f
s4ng <zalcls951201@gmail.com>
2022-11-16 20:35:09 +0900

    10버전 대응

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 원랜디 세이브 코드 로더
 
 ![ordsavecodeloader](https://user-images.githubusercontent.com/48652807/113292097-fb5eba00-932e-11eb-8ad6-b273d8d19c37.png)
+
+```
+// build
+pyinstaller --noconsole --icon="./static/icon.ico" "./src/app.py"
+```
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 buildOptions = dict(packages=['tkinter'])
 
-exe = [Executable('src/app.py', 
+exe = [Executable('./src/app.py', 
     base='Win32GUI', 
     targetName="OrdSaveCodeLoader.exe",
     icon="static/icon.ico"
@@ -10,7 +10,7 @@ exe = [Executable('src/app.py',
 
 setup(
     name='ordSaveCodeLoader', 
-    version="0.0.5",
+    version="0.0.6",
     author="s4ng",
     description="원랜디 세이브 코드 로더",
     options=dict(build_exe = buildOptions),
