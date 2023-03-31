13732c6f7af061b7f0ad360ce7282b4d2215541b
s4ng <zalcls951201@gmail.com>
2023-03-31 23:39:24 +0900

    11버전 업뎃

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ SHGFP_TYPE_CURRENT = 0
 buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
 ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
 
-ordPath = buf.value + '\\Warcraft III\\CustomMapData\\ORD10'
+ordPath = buf.value + '\\Warcraft III\\CustomMapData\\ORD11'
 userIdDirectoryPath = buf.value + '\\OrdSaveCodeLoader'
 userIdPath = buf.value + '\\OrdSaveCodeLoader\\userId.txt'
 
